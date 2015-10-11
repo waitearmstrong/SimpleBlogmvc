591cc3c7a566fd8e2c27e69e0deb8b7d5af4bf83
WaiteArmstrong <needlecinema@gmail.com>
2015-10-11 16:33:22 -0400

    Added basic routes and controllers and the ad in area.

---
+++ SimpleBlogMVC/App_Start/RouteConfig.cs
@@ -0,0 +1,22 @@
+﻿using SimpleBlogMVC.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace SimpleBlogMVC
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            var namespaces = new[] { typeof(PostsController).Namespace };
+
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapRoute("Login", "Login", new { Controller = "Auth", action = "Login" }, namespaces);
+            routes.MapRoute("Home", "", new { Controller = "Posts", action = "Index" }, namespaces);
+        }
+    }
+}
---
+++ SimpleBlogMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SimpleBlogMVC.MvcApplication" Language="C#" %>
